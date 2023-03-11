a388fa3e225a8c8b336091e716302c68bc9a7f0e
Reennee <116761539+Reennee@users.noreply.github.com>
2023-03-11 21:17:49 +0200

    course selection

---
+++ Group_coding_session-3/course-selection/courses.py
@@ -0,0 +1,31 @@
+# define a list of courses
+courses = [
+    "Python Programming",
+    "Web Development",
+    "Data Science",
+    "Machine Learning"
+]
+
+# display the menu of courses
+print("Select a course:")
+for i in range(len(courses)):
+    print(f"{i+1}. {courses[i]}")
+
+# prompt the user for their selection
+selection = int(input("Enter the number of the course you want to take: "))
+
+# execute the corresponding code block based on the user's selection
+if selection == 1:
+    print("You have selected Python Programming.")
+    # add code to start the Python Programming course
+elif selection == 2:
+    print("You have selected Web Development.")
+    # add code to start the Web Development course
+elif selection == 3:
+    print("You have selected Data Science.")
+    # add code to start the Data Science course
+elif selection == 4:
+    print("You have selected Machine Learning.")
+    # add code to start the Machine Learning course
+else:
+    print("Invalid selection.")
